388e260489e1f292b2f62916c8fb4b63a3d0f682
Kevin Kurian T <kevinthathathil@gmail.com>
2021-05-20 17:47:14 +0530

    Delete AutomationScripts/maplaunch directory

--- AutomationScripts/maplaunch/maps.py
+++
@@ -1,9 +0,0 @@
-#! python3
-import webbrowser,sys,pyperclip
-if len(sys.argv)>1:
-    address=' '.join(sys.argv[1:])
-else:
-    #to get address from the clipboard
-    address=pyperclip.paste()
-
-webbrowser.open('https://www.google.com/maps/place/'+address) 
