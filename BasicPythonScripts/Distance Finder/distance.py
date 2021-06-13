f9646df7b210af63118a860c86f018e3fdc00054
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-06-13 22:49:46 +0530

    Add files via upload

---
+++ BasicPythonScripts/Distance Finder/distance.py
@@ -0,0 +1,14 @@
+# Import following modules
+from geopy.geocoders import Nominatim  # pip install geopy
+from geopy.distance import geodesic
+# Link the user_agent to Google Map, so tat it can calculate latitude and longitude
+geolocator = Nominatim(user_agent="https://maps.google.com")
+fro = input("From: ")  # From which City
+to = input("To: ")  # To which city
+location = geolocator.geocode(fro)  # Fetch all the data of the city
+locate = geolocator.geocode(to)
+# Extract longitude and latitude
+newport_ri = (location.latitude, location.longitude)
+cleveland_oh = (locate.latitude, locate.longitude)
+# Display the distance in KiloMeters
+print(geodesic(newport_ri, cleveland_oh).km)
