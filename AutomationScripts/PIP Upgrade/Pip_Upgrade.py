2f2eda5d8bcae00b3ee87e7177dba1d200dbff65
rishav <merishujaiswal@gmail.com>
2021-06-16 14:50:47 +0530

    Adding PIP upgrade script

---
+++ AutomationScripts/PIP Upgrade/Pip_Upgrade.py
@@ -0,0 +1,19 @@
+import os
+import tkinter as tk
+
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 300, height = 350, bg = 'lightsteelblue2', relief = 'raised')
+canvas1.pack()
+
+label1 = tk.Label(root, text='Upgrade PIP', bg = 'lightsteelblue2')
+label1.config(font=('helvetica', 20))
+canvas1.create_window(150, 80, window=label1)
+
+def upgradePIP ():
+    os.system('start cmd /k python.exe -m pip install --upgrade pip') 
+    
+button1 = tk.Button(text='      Upgrade PIP     ', command=upgradePIP, bg='green', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 180, window=button1)
+
+root.mainloop()
---
+++ AutomationScripts/PIP Upgrade/README.md
@@ -0,0 +1,16 @@
+# This is a Python Script which will upgrade your PIP version in your OS. 
+
+## Requirements
+
+For this script to run you need to just follow the instruction.
+
+
+Run the program using command
+
+``` 
+$ python Pip_Upgrade.py
+```
+
+But this script is built in a way you don't need to open Command Prompt at all.
+
+-->Just run the script via any editor and you are good to go.
