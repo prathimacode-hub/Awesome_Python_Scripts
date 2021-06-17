ceb592da7f7ca51e097dccb6ce5988cf4a8ab6b2
Ashu-1309 <ashutoshkumarsaw1309@gmail.com>
2021-06-17 13:36:50 +0530

    This code is used to convert Celsius into Fahrenheit and Vice Versa

---
+++ BasicPythonScripts/Temperature_Converter/temperature_converter.py
@@ -0,0 +1,22 @@
+def Cel():
+    celsius = float(input("Enter the temperature in Celsius "))  # It will take user input
+    fahrenheit :float = (celsius * 9 / 5) + 32  # calculation part
+    print("Value in Fahrenheit ", fahrenheit)
+
+
+def Far():
+    fahrenheit = float(input("Enter the temperature in Fahrenheit "))  # It will take user input
+    celsius :float = (fahrenheit - 32) * 5 / 9  # calculation part
+    print(" Value in   Celsius", celsius)
+
+
+print(" To convert Temperatures")
+choice=int(input("Enter 1 to convert Celsius to Fahrenheit 0r Enter 2 for vice versa  "))
+
+if choice ==1 :
+    Cel()
+
+elif choice ==2 :
+    Far()
+else :
+    print(' Wrong choice ')
