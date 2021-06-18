f9a370bffec37a87e40fdc5ee414871a52215569
Adarsh <78082429+Adarsh357@users.noreply.github.com>
2021-06-18 19:48:13 +0530

    Update phone_number_info.py

--- BasicPythonScripts/Phone Number Info/phone_number_info.py
+++ BasicPythonScripts/Phone Number Info/phone_number_info.py
@@ -1,18 +1,14 @@
 # import the phonenumbers module to use it in this program
 import phonenumbers
-
-# From phonenumbers module we've to import 2 more libraries geocoder and carrier
+# geocoder: to know the specific location to that phone number 
+# carrier: to know the name of service provider of that phone
 from phonenumbers import geocoder
 from phonenumbers import carrier
+phone_number = phonenumbers.parse("Number with country code")
+service_provider = phonenumbers.parse("Number with country code") 
+# this will print the country name 
+print(geocoder.description_for_number(phone_number,'en'))
+#this will print the service provider name 
+print(carrier.name_for_number(service_provider,'en'))  
 
-# Take the user input
-PhoneNumber = input("Enter Phone Number: ")
-number = phonenumbers.parse(PhoneNumber)
-
-# Fetch the details from modules and store it in variables
-country = ("Country: " + geocoder.description_for_number(number, "en"))
-sp = ("Service Provider: " + carrier.name_for_number(number, "en"))
-
-# Print the output
-print(country)
-print(sp)
+                                      
