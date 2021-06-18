b6e375bd95379c9785343b2a8a6a33375268b5ce
Adarsh <78082429+Adarsh357@users.noreply.github.com>
2021-06-18 20:22:36 +0530

    Update guess_the_number.py

--- PyGamesScripts/Guess The Number/guess_the_number.py
+++ PyGamesScripts/Guess The Number/guess_the_number.py
@@ -1,26 +1,14 @@
-# import random library to use it in this program.
 import random
 
-# Variable num generates and stores a random number between 0 to 100.
-num = random.randrange(0, 100)
-guessCheck = "wrong"
-print("--Welcome to Guess The Number Game--")
+num = random.randint(1, 100)
+guess = None
 
-# Iterate using while loop
-while guessCheck == "wrong":
-    res = int(input("Please input a number between 0 and 100:"))
-    try:
-        val = int(res)
-    except ValueError:
-        print("This is not a valid integer. Please try again!")
-        continue
-    # Check whether the user input is high or low than the random number generated.
-    if val < num:
-        print("This is lower than actual number. Please try again!")
-    elif val > num:
-        print("This is higher than actual number. Please try again!")
+while guess != num:
+    guess = input("guess a number between 1 and 10: ")
+    guess = int(guess)
+
+    if guess == num:
+        print("congratulations! you won!")
+        break
     else:
-        # Final result will be printed with this message as the user input matches the random number generated
-        print("Hurray! you won, this is the correct number.")
-        guessCheck = "correct"
-print("Thank you for playing Guess The Number. See you again!")
+        print("nope, sorry. try again!")
