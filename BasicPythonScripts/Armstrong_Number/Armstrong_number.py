bf131627988cf504263559892a078bd0d5dcc6b9
Priyanshi Rai <83975944+Priyanshi-Rai@users.noreply.github.com>
2021-06-19 01:52:39 -0700

    Update Armstrong_number.py

--- BasicPythonScripts/Armstrong_Number/Armstrong_number.py
+++ BasicPythonScripts/Armstrong_Number/Armstrong_number.py
@@ -1,1 +1,20 @@
 
+#armstrong number
+number=input("Enter the number :- ")
+n=len(str(number))
+
+sum=0
+#we save the base as 
+number=int(number)
+local =int(number)
+
+while local > 0 : 
+    l_digit=local%10
+    sum =sum + l_digit**n
+    local//=10 
+    
+if number==sum:
+    print(number,"is an Armstrong Number")
+else:
+    print(number,"is not an Armstrong Number")
+    
