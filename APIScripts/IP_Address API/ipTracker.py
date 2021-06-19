dc51077ada7f82e1e36a9470bd8605eb5044e90a
its-me-debk007 <81604986+its-me-debk007@users.noreply.github.com>
2021-06-19 20:17:27 +0530

    Improved my code

--- APIScripts/IP_Address API/ipTracker.py
+++ APIScripts/IP_Address API/ipTracker.py
@@ -2,22 +2,25 @@ import requests #We'll Extract data using requests module
                 #If writing this statement gives you an error, then this module isn't installed in your system
                 #To install, type : pip install requests
 
-from simple_colors import * #A module to print bold/italic/underlined texts of different colours
+from simple_colors import * #A module to print bold/italic/underlined texts of different colours  
                             #To install, type : pip install simple-colors  
+                            #PS: Different types of formatted text will not work in IDLE
 
-url= "http://ip-api.com/json/" #This URL is related to info about IP Address
-url2= "http://8xgcd1gizl6s04w7a300wyxathwtrt5l.edns.ip-api.com/json/" #This URL is related to info about DNS Server
 
-ip=input(cyan("Enter any IP Address (Just hit enter if you want info about your current IP Address)\n","bold"))
+url = "http://ip-api.com/json/" #This URL is related to info about IP Address
+url2 = "http://8xgcd1gizl6s04w7a300wyxathwtrt5l.edns.ip-api.com/json/" #This URL is related to info about DNS Server
+
+ip = input(cyan("Enter any IP Address (Just hit enter if you want info about your current IP Address)\n","bold"))
 print()
 
-data=requests.get(url+ip).json() #No API key required
+data = requests.get(url+ip).json() #No API key required
     
-if ip=='': 
-    dns_data=requests.get(url2).json() #We can only find info about DNS Server of the IP Address that is currently been used 
+if ip == '': #if user did'nt entered anything (i.e. he wants info about his/her IP Address)
+           #We can find dns related info of the current IP Address only
+    dns_data = requests.get(url2).json() 
         
-if data['status']=='fail':
-    print(red("You entered a wrong IP Address ","bold"))
+if data['status'] == 'fail': # i.e. if an invalid IP Address is entered
+    print(red("You entered an invalid IP Address ","bold"))
         
 else:
     print(f'IP ADDRESS: {green(data["query"],"bold")} \n')
@@ -28,11 +31,11 @@ else:
     
     print(f'REGION: {green(data["regionName"],"bold")} \n')
     
-    o=str(data["lat"]) + " degrees"
-    print(f'LATITUDE: {green(o,"bold")} \n')
+    latitude = str(data["lat"]) + " degrees"
+    print(f'LATITUDE: {green(latitude,"bold")} \n')
     
-    q=str(data["lon"]) + " degrees"
-    print(f'LONGITUDE: {green(q,"bold")} \n')
+    longitude = str(data["lon"]) + " degrees"
+    print(f'LONGITUDE: {green(longitude,"bold")} \n')
     
     print(f'TIMEZONE: {green(data["timezone"],"bold")}\n')
     
@@ -40,17 +43,17 @@ else:
     
     print(f'ORGANISATION: {green(data["org"],"bold")} \n')
 
-    if ip=='':
+    if ip == '': #if user did'nt entered anything (i.e. he wants info about his/her IP)
             
         for i in range(len(dns_data["dns"]["geo"])):
-            if dns_data["dns"]["geo"][i]=='-':
+            if dns_data["dns"]["geo"][i] == '-':
                 break
             
         dns_name = dns_data["dns"]["geo"][i+2:]
         
         dns_country = dns_data["dns"]["geo"][:i-1]
         
-        j=dns_name +" (" + dns_country + ")"
+        j = dns_name +" (" + dns_country + ")"
         print(f'YOUR DNS SERVER: {green(j,"bold")} \n')
         
         print(f'IP ADDRESS OF YOUR DNS SERVER: {green(dns_data["dns"]["ip"],"bold")}')
