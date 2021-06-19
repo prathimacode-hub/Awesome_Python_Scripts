e8203ab48457e2ab88465f8d0721a2b6b4c01ea3
Priyanshi Rai <83975944+Priyanshi-Rai@users.noreply.github.com>
2021-06-19 03:26:08 -0700

    Update Armstrong_number.py

--- BasicPythonScripts/Armstrong_Number/Armstrong_number.py
+++ BasicPythonScripts/Armstrong_Number/Armstrong_number.py
@@ -1,10 +1,11 @@
 
 #armstrong number
-number=input("Enter the number :- ")
+number=input("Enter the number :- ")             #user input
+#storing the order of number in n
 n=len(str(number))
 
 sum=0
-#we save the base as 
+#changing the data type to int so that we can use it in looping
 number=int(number)
 local =int(number)
 
@@ -12,7 +13,7 @@ while local > 0 :
     l_digit=local%10
     sum =sum + l_digit**n
     local//=10 
-    
+#CONDITIONAL STATEMENTS    
 if number==sum:
     print(number,"is an Armstrong Number")
 else:
