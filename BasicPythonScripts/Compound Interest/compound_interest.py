82e684e2985616b769495cd0a1d978f14d0bb900
Vaibhav-Vipin <vbhvpn@gmail.com>
2021-06-29 12:13:46 +0530

    Compound Interest added

---
+++ BasicPythonScripts/Compound Interest/compound_interest.py
@@ -0,0 +1,26 @@
+# Calculating Compound interest
+
+print('How many years will you be saving?') 
+years = int(input('Enter years: '))
+
+print('\nHow much money is currently in your account?') 
+principal = float(input('Enter current amount in account: '))
+
+print('\nHow much money do you plan on invest in monthly?') 
+monthly_invest = float(input('Enter amount: '))
+
+print('\nWhat do you estimate will be the yearly interest of this investment?') 
+interest=float (input('Enter interest in decimal numbers (10% = 0.1): '))
+
+print(' ')
+
+monthly_invest = monthly_invest * 12
+final_amount = 0
+
+for i in range(0, years):
+    if final_amount == 0:
+        final_amount = principal
+    
+    final_amount = (final_amount + monthly_invest) * (1 + interest)
+    
+print('This is how much money you would have in your account after {} years: '.format(years) + str(final_amount))
