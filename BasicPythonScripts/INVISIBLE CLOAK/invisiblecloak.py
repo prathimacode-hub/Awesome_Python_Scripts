21e1f34bc7d517b33fe49002d9c76b71e184851e
Kartik Srivardhan <kartikshrivardhan@gmail.com>
2021-06-30 14:12:04 +0530

    Update invisiblecloak.py

--- BasicPythonScripts/INVISIBLE CLOAK/invisiblecloak.py
+++ BasicPythonScripts/INVISIBLE CLOAK/invisiblecloak.py
@@ -1,32 +1,102 @@
 import cv2
+
 import numpy as np
-cap=cv2.VideoCapture(0)
-back=cv2.imread('./image.jpg')
 
-while cap.isOpened():
-    ret, frame=cap.read()
+import time
+capture_video = cv2.VideoCapture("video.mp4")
 
-    if ret:
-        #conversion of RGB to HSV
-        hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-        #cv2.imshow('image.jpg',hsv)
-        red=np.uint8([[[0,0,255]]])
-        hsv_yellow=cv2.cvtColor(red, cv2.COLOR_BGR2HSV)
-        #print(hsv_red)
-        l_red=np.array([0,100,100])
-        u_red=np.array([10,255,255])
 
-        mask=cv2.inRange(hsv, l_red, u_red)
-        #cv2.imshow("mask",mask)
-        part1=cv2.bitwise_and(back,back,mask=mask)
-        #cv2.imshow('part2',part1)
-        mask=cv2.bitwise_not(mask)
+# give the camera to warm up
 
-        part2=cv2.bitwise_and(frame,frame,mask=mask)
-        cv2.imshow("cloak",part1+part2)
-        if cv2.waitKey(5)==ord('s'):
-            break
+time.sleep(1)
 
-cap.release()
-cap.destroyAllWindows()
+count = 0
 
+background = 0
+
+
+# capturing the background in range of 60
+# you should have video that have some seconds
+# dedicated to background frame so that it
+# could easily save the background image
+
+for i in range(60):
+
+    return_val, background = capture_video.read()
+
+    if return_val == False:
+
+        continue
+
+
+background = np.flip(background, axis=1)  # flipping of the frame
+
+
+# we are reading from video
+
+while (capture_video.isOpened()):
+
+    return_val, img = capture_video.read()
+
+    if not return_val:
+
+        break
+
+    count = count + 1
+
+    img = np.flip(img, axis=1)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+#-------------------------------------BLOCK----------------------------#
+
+# ranges should be carefully chosen
+
+# setting the lower and upper range for mask1
+
+lower_red = np.array([100, 40, 40])
+
+upper_red = np.array([100, 255, 255])
+
+mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+# setting the lower and upper range for mask2
+
+lower_red = np.array([155, 40, 40])
+
+upper_red = np.array([180, 255, 255])
+
+mask2 = cv2.inRange(hsv, lower_red, upper_red)
+
+#----------------------------------------------------------------------#
+
+# the above block of code could be replaced with
+
+# some other code depending upon the color of your cloth
+
+mask1 = mask1 + mask2
+
+# Refining the mask corresponding to the detected red color
+
+mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3),
+
+                                                        np.uint8), iterations=2)
+
+mask1 = cv2.dilate(mask1, np.ones((3, 3), np.uint8), iterations=1)
+
+mask2 = cv2.bitwise_not(mask1)
+
+# Generating the final output
+
+res1 = cv2.bitwise_and(background, background, mask=mask1)
+
+res2 = cv2.bitwise_and(img, img, mask=mask2)
+
+final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
+
+cv2.imshow("INVISIBLE MAN", final_output)
+
+k = cv2.waitKey(10)
+
+if k == 27:
+
+    break
