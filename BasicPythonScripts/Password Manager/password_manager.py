6f0c9e1ad4db0fff15285d3430f4bea650392216
Neel Shah <71593494+neelshah2409@users.noreply.github.com>
2021-06-30 17:38:47 +0530

    Update password_manager.py

--- BasicPythonScripts/Password Manager/password_manager.py
+++ BasicPythonScripts/Password Manager/password_manager.py
@@ -1,12 +1,12 @@
 from cryptography.fernet import Fernet
 
-
+#generating the key
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
 
-
+#verifying the key
 def load_key():
     file = open("key.key", "rb")
     key = file.read()
@@ -17,7 +17,7 @@ def load_key():
 key = load_key()
 fer = Fernet(key)
 
-
+#for viewing all the passwords
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
@@ -26,7 +26,7 @@ def view():
             print("User:", user, "| Password:",
                   fer.decrypt(passw.encode()).decode())
 
-
+#for adding the passwords
 def add():
     name = input('Account Name: ')
     pwd = input("Password: ")
@@ -34,7 +34,7 @@ def add():
     with open('passwords.txt', 'a') as f:
         f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
-
+#printing the statement continously for adding or viewing unless user press quit
 while True:
     mode = input(
         "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
