063624592b526aebe5a2d39b46110839b9ccff79
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-07-17 23:31:11 +0530

    Update Spotify_Song_Lyrics_Extractor.py

--- WebScrapingScripts/Spotify Song Lyrics Extractor/Spotify_Song_Lyrics_Extractor.py
+++ WebScrapingScripts/Spotify Song Lyrics Extractor/Spotify_Song_Lyrics_Extractor.py
@@ -10,71 +10,71 @@ spotify_client_secret = None
 genius_client_secret = None
 
 class GetLyrics:
-    def __init__(self):
-        self.title = None
-        self.artist = None
-        self.lyrics = None
-        self.source = None
-        self.query = None
+	def __init__(self):
+		self.title = None
+		self.artist = None
+		self.lyrics = None
+		self.source = None
+		self.query = None
 
-    def google_lyrics(self, query):
-    	'''This function will scrape lyrics of a particular song from the google along
-    	   with its title,author and source of the song and display
-    	   it in console'''
-        query = str(query)
-        try:
-            url = "https://www.google.com/search?q=" + query.replace(" ", "+") + "+lyrics"
-            print(url)
+	def google_lyrics(self, query):
+		'''This function will scrape lyrics of a particular song from the google along
+		   with its title,author and source of the song and display
+		   it in console'''
+		query = str(query)
+		try:
+			url = "https://www.google.com/search?q=" + query.replace(" ", "+") + "+lyrics"
+			print(url)
 
-            r = requests.get(url)
-            htmlcontent = r.content
-            html_content = BeautifulSoup(htmlcontent, "lxml")
+			r = requests.get(url)
+			htmlcontent = r.content
+			html_content = BeautifulSoup(htmlcontent, "lxml")
 
-            title = str(html_content.find("span", class_="BNeawe tAd8D AP7Wnd"))
-            title = re.sub(r"(<.*?>)*", "", title).replace("[", "").replace("]", "")
-            print("Title:",title)
+			title = str(html_content.find("span", class_="BNeawe tAd8D AP7Wnd"))
+			title = re.sub(r"(<.*?>)*", "", title).replace("[", "").replace("]", "")
+			print("Title:",title)
 
-            artist = html_content.find_all("span", class_="BNeawe s3v9rd AP7Wnd")
-            artist = str(artist[1])
-            artist = re.sub(r"(<.*?>)*", "", artist).replace("[", "").replace("]", "")
-            print("Artist:",artist)
+			artist = html_content.find_all("span", class_="BNeawe s3v9rd AP7Wnd")
+			artist = str(artist[1])
+			artist = re.sub(r"(<.*?>)*", "", artist).replace("[", "").replace("]", "")
+			print("Artist:",artist)
 
-            lyrics = html_content.find_all("div", class_="BNeawe tAd8D AP7Wnd")
-            lyrics = str(lyrics[2])
-            lyrics = re.sub(r"(<.*?>)*", "", lyrics).replace("[", "").replace("]", "")
+			lyrics = html_content.find_all("div", class_="BNeawe tAd8D AP7Wnd")
+			lyrics = str(lyrics[2])
+			lyrics = re.sub(r"(<.*?>)*", "", lyrics).replace("[", "").replace("]", "")
 
-            source = str(html_content.find("span", class_="uEec3 AP7Wnd"))
-            source = re.sub(r"(<.*?>)*", "", source).replace("[", "").replace("]", "")
+			source = str(html_content.find("span", class_="uEec3 AP7Wnd"))
+			source = re.sub(r"(<.*?>)*", "", source).replace("[", "").replace("]", "")
 
-            if lyrics is None or artist is None or title is None or source is None:
-                raise Exception("Something went wrong. No lyrics yielded. ")
+			if lyrics is None or artist is None or title is None or source is None:
+				raise Exception("Something went wrong. No lyrics yielded. ")
 
-            self.title = title  # Name of the track
-            self.artist = artist  # Name of the artist
-            self.lyrics = lyrics  # Lyrics of the track
-            self.source = source  # Source of the lyrics
-            self.query = query  # Query requested by the user
-        except:
-            raise Exception
+			self.title = title 
+			self.artist = artist 
+			self.lyrics = lyrics  
+			self.source = source  
+			self.query = query
+		except:
+			raise Exception
 
 
 def get_lyrics(full_title):
 	'''This function will call class and its function to get lyrics'''
-    query_title = str(full_title)  
-    query_title = re.sub(r'[^\w]', ' ', query_title)
-    query_title = re.sub(' +', ' ', query_title)
+	query_title = str(full_title)  
+	query_title = re.sub(r'[^\w]', ' ', query_title)
+	query_title = re.sub(' +', ' ', query_title)
 
-    ly = GetLyrics()
-    try:
-        ly.google_lyrics(query_title)
-    except:
-    	print("Not found")
-    lyrics = f"{ly.lyrics} Lyrics provided by {ly.source}"
-    return lyrics
+	ly = GetLyrics()
+	try:
+		ly.google_lyrics(query_title)
+	except:
+		print("Not found")
+	lyrics = f"{ly.lyrics} Lyrics provided by {ly.source}"
+	return lyrics
 
 def get_song_info(url,sp):
 	'''This function will extract all spotify songs from the playlist url
-	    and make a list of song names and return it'''
+		and make a list of song names and return it'''
 	songs = []
 	print(" \nGetting all tracks from the Spotify playlist. ")
 	playlist_items = None
@@ -103,24 +103,24 @@ def checkCredential():
 	'''This function will check credentials to get access of spotify 
 	   and thier playlist songs'''
 	try:
-	    api_credentials = open("API_Credentials.json", "r")
-	    api_data = json.load(api_credentials)
-	    spotify_client_id = api_data["API_Credentials"]["Spotify_Client_ID"]
-	    spotify_client_secret = api_data["API_Credentials"]["Spotify_Client_Secret"]
-	    genius_client_secret = api_data["API_Credentials"]["Genius_Client_Access_Token"]
+		api_credentials = open("Related/API_Credentials.json", "r")
+		api_data = json.load(api_credentials)
+		spotify_client_id = api_data["API_Credentials"]["Spotify_Client_ID"]
+		spotify_client_secret = api_data["API_Credentials"]["Spotify_Client_Secret"]
+		genius_client_secret = api_data["API_Credentials"]["Genius_Client_Access_Token"]
 
-	    if spotify_client_id == "None" or spotify_client_secret == "None" or genius_client_secret == "None":
-	        print(" [x] Any field in 'API_Credentials.json' is empty. Please read "
-	              "'Instructions.txt' before executing the code. Press any key to exit. \n ")
-	        input()
-	        exit()
-	    else:
-	        auth_manager = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
-	        sp = spotipy.Spotify(auth_manager=auth_manager)
-	        return sp
+		if spotify_client_id == "None" or spotify_client_secret == "None" or genius_client_secret == "None":
+			print(" [x] Any field in 'API_Credentials.json' is empty. Please read "
+				  "'Instructions.txt' before executing the code. Press any key to exit. \n ")
+			input()
+			exit()
+		else:
+			auth_manager = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
+			sp = spotipy.Spotify(auth_manager=auth_manager)
+			return sp
 	except KeyError:
-	    print(" [x] Seems like 'API_Credentials.json''s key values had been changed. Cannot fetch "
-	          "details. Press any key to exit. \n ")
+		print(" [x] Seems like 'API_Credentials.json''s key values had been changed. Cannot fetch "
+			  "details. Press any key to exit. \n ")
 	
 
 '''Main program starts from here'''
@@ -143,8 +143,8 @@ if choice=='1':
 		try:
 			lyrics = get_lyrics(song)
 			'''To store lyrics in the lyrics folder
-		    where choosen song lyrics will be save as .txt file'''
-			f = open(f"Lyrics/{song}.txt", "w+", encoding='utf-8')
+			where choosen song lyrics will be save as .txt file'''
+			f = open(f"Related/Lyrics/{song}.txt", "w+", encoding='utf-8')
 			f.write(lyrics)
 			f.close()
 			print(" [✓] Successful. ")
@@ -158,8 +158,8 @@ if choice=='2':
 	try:
 		lyrics = get_lyrics(spotify_song)
 		'''To store lyrics in the lyrics folder
-		    where each song lyrics will be save as .txt file'''
-		f = open(f"Lyrics/{spotify_song}.txt", "w+", encoding='utf-8')
+			where each song lyrics will be save as .txt file'''
+		f = open(f"Related/Lyrics/{spotify_song}.txt", "w+", encoding='utf-8')
 		f.write(lyrics)
 		f.close()
 		print("[✓] Successful. ")
