660aab3e164e2382b29fafacc8bd76f13bcbedf5
Gaurav1401 <gk.bhagat14@gmail.com>
2021-07-23 01:11:14 +0530

    Called whle data from the API gaurav1401

--- APIScripts/Covid 19 Tracker/covid_updates.py
+++ APIScripts/Covid 19 Tracker/covid_updates.py
@@ -4,10 +4,11 @@ import json
 # API link:- https://disease.sh/v3/covid-19/all
 
 response = requests.get('https://disease.sh/v3/covid-19/all')
-# print(response.status_code) #To check whether API call is successful or not
+# print(response.status_code) #To check whether API call is successful(200) or not(404)
 # print(response.text) # To access all the content of the website
 # print(response.url) # url of the website to which we made our API call
 
 python_data = json.loads(response.text)
 print(type(python_data))
-print(python_data)
+for key in python_data.keys():
+    print(f'{key} : {python_data[key]}')
