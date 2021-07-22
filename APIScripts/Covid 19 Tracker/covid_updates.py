93c2f151f948d89d5afbab26eb22f259cf982178
Gaurav1401 <gk.bhagat14@gmail.com>
2021-07-23 00:21:50 +0530

    Added url response attribute gaurav1401

--- APIScripts/Covid 19 Tracker/covid_updates.py
+++ APIScripts/Covid 19 Tracker/covid_updates.py
@@ -3,5 +3,6 @@ import requests
 # API link:- https://disease.sh/v3/covid-19/all
 
 response = requests.get('https://disease.sh/v3/covid-19/all')
-print(response.status_code) #To check whether API call is successful or not
-print(response.text) # To access all the content of the website
+# print(response.status_code) #To check whether API call is successful or not
+# print(response.text) # To access all the content of the website
+print(response.url) # url of the website to which we made our API call
