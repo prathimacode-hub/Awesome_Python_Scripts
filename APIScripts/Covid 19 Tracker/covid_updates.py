6e22f8ec4b11d93ac5ffc7382f93acd634e6a217
Gaurav1401 <gk.bhagat14@gmail.com>
2021-07-23 00:17:44 +0530

    Added basic response attributes gaurav1401

---
+++ APIScripts/Covid 19 Tracker/covid_updates.py
@@ -0,0 +1,7 @@
+import requests
+
+# API link:- https://disease.sh/v3/covid-19/all
+
+response = requests.get('https://disease.sh/v3/covid-19/all')
+print(response.status_code) #To check whether API call is successful or not
+print(response.text) # To access all the content of the website
