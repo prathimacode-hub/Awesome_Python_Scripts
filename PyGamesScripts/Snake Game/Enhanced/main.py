d3a6afdbc27bc79b96ddb99a3a5421f8a735bc9f
AlexAdvent <62216338+AlexAdvent@users.noreply.github.com>
2021-07-22 18:26:29 +0530

    Add comment

--- PyGamesScripts/Snake Game/Enhanced/main.py
+++ PyGamesScripts/Snake Game/Enhanced/main.py
@@ -1,21 +1,23 @@
-from turtle import Screen
+# import class and library
+from turtle import Screen  
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 import time
 
-
+# setupping screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-
+# creating snake player from snake class
 snake_player = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+# listening to keyboard event
 screen.listen()
 screen.onkey(snake_player.up,"Up")
 screen.onkey(snake_player.down,"Down")
@@ -23,12 +25,18 @@ screen.onkey(snake_player.left,"Left")
 screen.onkey(snake_player.right,"Right")
 
 
+# check for game status
 game_over = False
 
+# to game keep running
 while not game_over:
     screen.update()
+#     create delay in game
     time.sleep(0.1)
+#     calling move function
     snake_player.move()
+    
+#     check snake eat food
     if snake_player.snakes_bit[0].distance(food) < 15:
         food.refresh()
         scoreboard.incr_score()
@@ -38,8 +46,7 @@ while not game_over:
 
 #     detect collision with wall
     if snake_player.snakes_bit[0].xcor() > 280 or snake_player.snakes_bit[0].xcor() < -280 or snake_player.snakes_bit[0].ycor() > 280 or snake_player.snakes_bit[0].ycor() < -280:
-        # scoreboard.game_over()
-        # game_over = True
+
         scoreboard.reset()
         snake_player.reset()
 
@@ -50,7 +57,6 @@ while not game_over:
         elif snake_player.snakes_bit[0].distance(bit) < 10:
             scoreboard.reset()
             snake_player.reset()
-            # game_over = True
-            # scoreboard.game_over()
+
 
 screen.exitonclick()
