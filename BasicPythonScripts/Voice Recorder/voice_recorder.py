e4132de3d4459e0ef510ce0d3f4a5395c9f5f25f
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2021-07-24 22:54:07 +0530

    added comments in code

--- BasicPythonScripts/Voice Recorder/voice_recorder.py
+++ BasicPythonScripts/Voice Recorder/voice_recorder.py
@@ -2,18 +2,23 @@ import sounddevice as sd
 import wavio as wv
 import random
 
+# Sampling frequency
 freq = 44100
 
-duration = 10
+# Recording duration in seconds
+duration = 20
 
 print("Recording...")
 
+# Start recorder with the given values of duration and sample frequency
 recording = sd.rec(int(duration * freq), samplerate=freq, channels=2)
 
+# Record audio for the given number of seconds
 sd.wait()
 
 name = random.randint(1, 10000)
 
+# Convert the NumPy array to audio file
 wv.write(f"recording{name}.wav", recording, freq, sampwidth=2)
 
 print("Recording Completed...")
