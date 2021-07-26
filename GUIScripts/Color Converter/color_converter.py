95f23dcfbf9adf7b8cbc398e21a4348502f46bac
Akash Ramanand Rajak <aakashrajak02@gmail.com>
2021-07-26 19:27:02 +0530

    Enhanced Color Converter with RGB to LAB Conversion

--- GUIScripts/Color Converter/color_converter.py
+++ GUIScripts/Color Converter/color_converter.py
@@ -6,6 +6,7 @@ from tkinter import *  # from tkinter we import everything
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+import tkinter.messagebox as mbox
 import colorsys
 
 # Main Window
@@ -290,6 +291,112 @@ def def_hsv_to_rgb():
                       borderwidth=3, relief="raised")
     convertb.place(x=380, y=600)
 
+# function to convert from rgb to lab
+def def_rgb_to_lab():
+    # function for convert button
+    def convert_fun():
+        input_rgb = f_entry.get()
+        list = (input_rgb.rstrip().split(' '))
+
+        num = 0
+        # created list RGB and initialized with 0
+        RGB = [0, 0, 0]
+        for value in list:
+            value = float(value) / 255
+            if value > 0.04045:
+                value = ((value + 0.055) / 1.055) ** 2.4
+            else:
+                value = value / 12.92
+            RGB[num] = value * 100
+            num = num + 1
+        XYZ = [0, 0, 0,]
+        # converted all the three R, G, B to X, Y, Z
+        X = RGB[0] * 0.4124 + RGB[1] * 0.3576 + RGB[2] * 0.1805
+        Y = RGB[0] * 0.2126 + RGB[1] * 0.7152 + RGB[2] * 0.0722
+        Z = RGB[0] * 0.0193 + RGB[1] * 0.1192 + RGB[2] * 0.9505
+        # rounded off the values upto 4 decimal digit
+        XYZ[0] = round(X, 4)
+        XYZ[1] = round(Y, 4)
+        XYZ[2] = round(Z, 4)
+        XYZ[0] = float(XYZ[0]) / 95.047  # ref_X =  95.047   Observer= 2°, Illuminant= D65
+        XYZ[1] = float(XYZ[1]) / 100.0  # ref_Y = 100.000
+        XYZ[2] = float(XYZ[2]) / 108.883  # ref_Z = 108.883
+        num = 0
+        for value in XYZ:
+            if value > 0.008856:
+                value = value ** (0.3333333333333333)
+            else:
+                value = (7.787 * value) + (16 / 116)
+            XYZ[num] = value
+            num = num + 1
+
+        # formed Lab list and initialize with 0
+        Lab = [0,0, 0]
+        # found L, A, and B
+        L = (116 * XYZ[1]) - 16
+        a = 500 * (XYZ[0] - XYZ[1])
+        b = 200 * (XYZ[1] - XYZ[2])
+        # rounded off to 4 decimal digit
+        Lab[0] = round(L,4)
+        Lab[1] = round(a,4)
+        Lab[2] = round(b,4)
+        x = str(Lab[0]) + " " + str(Lab[1]) + " " + str(Lab[2])
+
+        s_entry.delete(0, END)
+        s_entry.insert(0, x)
+
+        red = int(list[0])
+        green = int(list[1])
+        blue = int(list[2])
+        x1 = '#%02x%02x%02x' % (red, green, blue)
+
+        # previewed
+        preview_text.configure(bg=str(x1))
+        top1.configure(bg=str(x1))
+        top2.configure(bg=str(x1))
+        # mbox.showinfo("Brightness", "Brightness Level  :  " + str(Lab[0]))
+
+
+    # created frame1 window
+    frame1 = Tk()
+    frame1.title('RGB to LAB')
+    frame1.geometry('1000x700')
+
+    # top label
+    start1 = tk.Label(frame1, text="RGB to LAB", font=("Arial", 50), fg="magenta")  # same way bg
+    start1.place(x=290, y=10)
+
+    # for preview
+    preview_text = tk.Text(frame1, height=15, width=60, font=("Arial", 15), bg="white", borderwidth=3, relief="solid")
+    preview_text.place(x=150, y=100)
+
+    # preview1 label
+    top1 = Label(frame1, text="PREVIEW AREA", font=("Arial", 40), fg="black", bg="white")  # same way bg
+    top1.place(x=270, y=180)
+
+    # preview2 label
+    top2 = Label(frame1, text="Color will be previewed here...", font=("Arial", 25), fg="black",bg="white")  # same way bg
+    top2.place(x=265, y=280)
+
+    # label for color name ---------------------------------------------------------------------------------
+    f_label = tk.Label(frame1, text="RGB Code", font=("Arial", 35), fg="brown")  # same way bg
+    f_label.place(x=180, y=470)
+
+    f_entry = Entry(frame1, font=("Arial", 30), width=10, border=2, bg="light yellow", fg="brown")
+    f_entry.place(x=180, y=530)
+
+    # label for color name ---------------------------------------------------------------------------------
+    s_label = tk.Label(frame1, text="LAB Code", font=("Arial", 35), fg="brown")  # same way bg
+    s_label.place(x=550, y=470)
+
+    s_entry = Entry(frame1, font=("Arial", 30), width=15, border=2, bg="light yellow", fg="brown")
+    s_entry.place(x=500, y=530)
+
+    # created a button for hex to rgb conversion
+    convertb = Button(frame1, text='CONVERT', command=convert_fun, font=("Arial", 25), bg="light green", fg="blue",borderwidth=3, relief="raised")
+    convertb.place(x=380, y=600)
+
+
 # image on the main window
 path = "Images/color.png"
 # Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
@@ -313,6 +420,10 @@ rgbtohexb.place(x=100, y=200)
 # created a button for hex to rgb conversion
 hextorgbb = Button(frame, text='HEX to RGB', command=def_hex_to_rgb, font=("Arial", 25), bg = "orange", fg = "blue", borderwidth=3, relief="raised")
 hextorgbb.place(x=670, y=200)
+
+# created a button for rgb to lab conversion
+rgbtolabb = Button(frame, text='RGB to LAB', command=def_rgb_to_lab, font=("Arial", 25), bg = "orange", fg = "blue", borderwidth=3, relief="raised")
+rgbtolabb.place(x=380, y=350)
 
 # created a button for rgb to hsv conversion
 rgbtohsvb = Button(frame, text='RGB to HSV', command=def_rgb_to_hsv, font=("Arial", 25), bg = "orange", fg = "blue", borderwidth=3, relief="raised")
