685cbce7e7cfc557ba583e259f9563d8e0f5b3bf
Ashi-00 <76102137+Ashi-00@users.noreply.github.com>
2021-10-13 14:02:11 +0530

    Add files via upload

---
+++ PyGamesScripts/MADLIBS_Game/avengers.py
@@ -0,0 +1,27 @@
+#Avengers Madlib Game
+def madlib():
+    #Defining Blanks
+    Adj1=input("Adjective = ")
+    Adj2=input("Adjective = ")
+    Adj3=input("Adjective = ")
+    verb1=input("Verb in present tense = ")
+    verb4=input("Verb in continuous tense = ")
+    FamousPersonality1=input("FamousPersonality = ")
+    FamousPersonality2=input("FamousPersonality = ")
+
+    print("The Avengers Template")
+    print("The Harry Potter Template")
+
+
+    #MADLIB STORY
+    madlib=f""" And with the snap of the finger, everything started to {verb1} back. Hulk aka Bruce Banner was thrown back by the recoiling
+    power of the gauntlet. Spiderman aka {Adj1} Peter thrived back to life and started blabbering about his time in that quantum of time
+    with {FamousPersonality1}, Tony aka Iron man was happily in tears listening to this little boy he had been missing all these five years.
+    Ah! but Thanos equivalent to our {FamousPersonality2} was still standing alive with all his {Adj2} plans in action with his daughter 
+    Nebula (from the past). And to the rescue came our {Adj3} Captain Marvel and the troops of Waganda {verb4} and fighting the warship 
+    of Thanos!! """
+
+
+    
+    print(madlib)
+    print()
