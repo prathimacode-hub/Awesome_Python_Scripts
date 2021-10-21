72ebd586405868cf38d02c8f8d8d50c9435c7888
atharva bhandvalkar <70326109+a-tharva@users.noreply.github.com>
2021-10-21 19:42:49 +0530

    Update duckduckgo_it.py

--- APIScripts/DuckDuckGo it/duckduckgo_it.py
+++ APIScripts/DuckDuckGo it/duckduckgo_it.py
@@ -1,8 +1,4 @@
-"""
-This module will take an input string and print the search result. 
-"""
-import requests
-
+import requests # This module will help extract data
 
 def main():
     
@@ -11,26 +7,28 @@ def main():
             print('\n\n')
             search_for = input('search for: ')      # Input string to search 
 
-            response = requests.get(f'http://api.duckduckgo.com/?q={search_for}&format=json')   # Will send request with search_for variable
+            response = requests.get(f'http://api.duckduckgo.com/?q={search_for}&format=json')   # Send request with {search_for} variable
             response = response.json()      # The result will be in json format
+                                            # it will be in key, value pair which will be easy to find answers
 
-            # Printing search result here
-            print('Heading:',response['Heading'])
-            print('\nAbstract:',response['Abstract'])
+            # Printing search result here we will print the values of keys inside brackets
+            print('Heading:',response['Heading'])    
+            print('\nAbstract:',response['Abstract'])  
             print('\nAbstract source:',response['AbstractSource'])
-            print('\nAbstract text:',response['AbstractText'])
-            print('\nDefinition:',response['Definition'])
+            print('\nAbstract text:',response['AbstractText'])    
+            print('\nDefinition:',response['Definition'])    
             print('Related Topic:')
             
-            related_no = 2          # Will show only no of text in realted topics
-            for i in response['RelatedTopics']:     # Will loop through Text in RelatedTopics
+            related_no = 2          # Show only no of text in realted topics
+                                    # Sometimes tere are less related topics so keep related no low
+            for i in response['RelatedTopics']:     # Loop through Text in RelatedTopics
                 if related_no <= 0:
                     break
                 print(i['Text'])
                 related_no = related_no - 1
                 
-        
         except KeyboardInterrupt:       # ctrl+c will call KeyboardInterrupt and while loop will stop
+                                        # it will stop execution of code and will print Exiting..
             print('\nExiting..')
             break
     
