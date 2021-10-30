ffda0e63215e8a991a270e848ce70281129dcead
Khushi Sharma <77405013+Khushi-sharma07@users.noreply.github.com>
2021-10-30 16:34:50 +0530

    Update Voice calculator.py

--- GUIScripts/Voice Calculator/Voice calculator.py
+++ GUIScripts/Voice Calculator/Voice calculator.py
@@ -10,6 +10,8 @@ def click(value):
     ex = entryField.get()
     answer = ''
 
+# given the commands to show what the various buttons will do
+
     try:
         if value == 'C':
             ex = ex[0:len(ex) - 1]
@@ -112,6 +114,8 @@ def hcf(a,b):
     h=math.gcd(a,b)
     return h
 
+# Voice command operations
+
 operations={'ADD':add,'ADDITION':add,'SUM':add,'PLUS':add,
             'SUBTRACTION':sub , 'DIFFERENCE':sub , 'MINUS':sub , 'SUBTRACT':sub,
             'PRODUCT': mul, 'MULTIPLICATION': mul,'MULTIPLY': mul,
@@ -128,6 +132,8 @@ def findNumbers(t):
         except ValueError:
             pass
     return l
+
+# the function will give the audio sound when you click the mic button
 
 def audio():
     mixer.music.load('Media/mictone.mp3')
@@ -154,8 +160,11 @@ def audio():
         except:
             pass
 
+# The gui display of calculator
+
 root = Tk()
 root.title('Voice Calculator')
+# Screen display colour and dimensions
 root.config(bg='grey')
 root.geometry('680x486+100+100')
 
@@ -170,6 +179,7 @@ micImage = PhotoImage(file='Media/microphone.png')
 micButton = Button(root, image=micImage, bd=0, bg='grey', activebackground='orange',command=audio)
 micButton.grid(row=0, column=7)
 
+#button list
 button_text_list = ["C", "DEL", "√", "+", "π", "sinθ", "cosθ", "tanθ",
                     "1", "2", "3", "-", "2π", "cosh", "tanh", "sinh",
                     "4", "5", "6", "*", chr(8731), "x\u02b8", "x\u00B3", "x\u00B2",
