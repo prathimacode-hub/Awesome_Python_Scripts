71989b22e824ad9dea7e1330a542646e52cf12b8
nirbhayChaplot <nirc34@gmail.com>
2021-12-27 10:43:58 +0530

    UPDATE README

--- ImageProcessingScripts/Image To Text Converter/image_to_text_converter.py
+++ ImageProcessingScripts/Image To Text Converter/image_to_text_converter.py
@@ -4,6 +4,7 @@ import PySimpleGUI as sg
 from PIL import Image
 import pytesseract
 
+# include the file types that should be allowed in your app
 file_types = [
     ("PNG (*.png)", "*.png"),
     ("JPEG (*.jpg)", "*.jpg"),
@@ -16,10 +17,11 @@ def ocr_core(filename):
     This function will handle the core OCR processing of images.
     """
     pytesseract.pytesseract.tesseract_cmd = r'<Put your tessract-ocr exe path here>'
+    # the function 'image_to_string' extracts all the text from the image
     text = pytesseract.image_to_string(Image.open(filename))
     return text
 
-
+# the ui is rendered as a function call and anything defined inside the function is rendered in the window
 def main():
     col1 = [[sg.Image(key="-IMAGE-")]]
     col2 = [[
@@ -28,6 +30,7 @@ def main():
         sg.FileBrowse(file_types=file_types),
         sg.Button("Load Image"),
     ]]
+    # see the readme for understanding Layout
     layout = [[
         sg.Column(col1, element_justification='c'),
         sg.Column(col2, element_justification='c'),
@@ -45,16 +48,22 @@ def main():
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
         if event == "Load Image":
+            # if the load image button is clicked
             filename = values["-FILE-"]
             if os.path.exists(filename):
                 image = Image.open(values["-FILE-"])
                 image.thumbnail((400, 400))
+                # convert the image to bytestream
                 bio = io.BytesIO()
+                # save the image as PNG
                 image.save(bio, format="PNG")
+                # update the ui to show the loaded image
                 window["-IMAGE-"].update(data=bio.getvalue())
         if event == "Load Text":
+            # The image is taken and its file path is passed into the ocr_core() function
             filename = values["-FILE-"]
             if os.path.exists(filename):
+                # update the text based on the returned value of the function
                 window["-TEXTBOX-"].update(ocr_core(filename))
 
     window.close()
