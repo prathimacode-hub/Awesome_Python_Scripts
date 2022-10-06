bef7b06977f7edd98f2d121ac82aa963889d6a2c
Vivek Kumar Singh <71426368+vivekthedev@users.noreply.github.com>
2022-10-06 21:53:46 +0530

    Update script.py

--- BasicPythonScripts/Emoji To Text/script.py
+++ BasicPythonScripts/Emoji To Text/script.py
@@ -1,4 +1,7 @@
 import unicodedata
 
+# Get the emoji from user input as a string
 em = input('Enter Emoji : ')
+
+# return the encoded name of the emoji from the metadata
 print(unicodedata.name(em[0]))
